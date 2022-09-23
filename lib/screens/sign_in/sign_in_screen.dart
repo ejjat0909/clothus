2593574ad6598fd06a158ebfda6205e4dfab8e74
Izzat Rizal <86939634+ejjat0909@users.noreply.github.com>
2022-09-23 08:39:54 +0800

    remove text form bloc

--- lib/screens/sign_in/sign_in_screen.dart
+++ lib/screens/sign_in/sign_in_screen.dart
@@ -1,7 +1,12 @@
+import 'package:clothus/bloc/login_bloc.dart';
 import 'package:clothus/components/input_decoration.dart';
 import 'package:clothus/components/loading_dialog.dart';
 import 'package:clothus/constant.dart';
 import 'package:clothus/form_bloc/login_form_bloc.dart';
+import 'package:clothus/helpers/secure_shared_preferences.dart';
+import 'package:clothus/models/default_response_model.dart';
+import 'package:clothus/models/user/login_request_model.dart';
+import 'package:clothus/models/user/user_response_model.dart';
 import 'package:clothus/screens/home_page/home_page_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_bloc/flutter_form_bloc.dart';
@@ -15,6 +20,11 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  LoginBloc loginBloc = LoginBloc();
+  LoginRequestModel _loginRequestModel = LoginRequestModel();
+  var emailController = TextEditingController();
+  var passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,140 +33,154 @@ class _SignInScreenState extends State<SignInScreen> {
         backgroundColor: Colors.transparent,
         elevation: 0,
       ),
-      body: BlocProvider(
-        create: (context) => LoginFormBloc(),
-        child: Builder(builder: (context) {
-          // Access form bloc
-          final loginFormBloc = context.read<LoginFormBloc>();
-          return FormBlocListener<LoginFormBloc, String, String>(
-            onSubmitting: ((context, state) {
-              LoadingDialog.show(context);
-            }),
-            onSubmissionFailed: (context, state) {
-              LoadingDialog.hide(context);
-            },
-            onSuccess: (context, state) {
-              LoadingDialog.hide(context);
-
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const HomePageScreen()));
-            },
-            onFailure: (context, state) {
-              LoadingDialog.hide(context);
-
-              ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(content: Text(state.failureResponse!)));
-            },
-            child: Container(
-              width: double.infinity,
-              child: Column(
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          children: [
+            RichText(
+              text: TextSpan(
+                text: "Cloth",
+                style: TextStyle(
+                  color: ColorConstant.primaryColor,
+                  fontWeight: FontWeight.normal,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 60,
+                ),
                 children: [
-                  RichText(
-                    text: TextSpan(
-                      text: "Cloth",
-                      style: TextStyle(
-                        color: ColorConstant.primaryColor,
-                        fontWeight: FontWeight.normal,
-                        fontStyle: FontStyle.italic,
-                        fontSize: 60,
-                      ),
-                      children: [
-                        TextSpan(
-                          text: "Us",
-                          style: TextStyle(
-                            color: ColorConstant.primaryColor,
-                            fontWeight: FontWeight.bold,
-                            fontStyle: FontStyle.normal,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  // Title
-                  Text(
-                    "Welcome back to Clothus, \n You've been missed",
-                    textAlign: TextAlign.center,
+                  TextSpan(
+                    text: "Us",
                     style: TextStyle(
                       color: ColorConstant.primaryColor,
-                      fontSize: 16,
-                    ),
-                  ),
-                  const SizedBox(height: 50),
-                  // Form Card
-                  Expanded(
-                    child: Container(
-                      width: double.infinity,
-                      padding: const EdgeInsets.all(20),
-                      decoration: BoxDecoration(
-                        color: ColorConstant.white,
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(25),
-                          topRight: Radius.circular(25),
-                        ),
-                      ),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10),
-                        child: SingleChildScrollView(
-                          physics: const BouncingScrollPhysics(),
-                          child: Column(
-                            children: [
-                              const SizedBox(height: 10),
-                              Form(
-                                child: Column(
-                                  children: [
-                                    TextFieldBlocBuilder(
-                                      textFieldBloc: loginFormBloc.email,
-                                      keyboardType: TextInputType.emailAddress,
-                                      decoration: textFieldInputDecoration(
-                                          "Email", "ex: izzat@ump.com"),
-                                    ),
-                                    const SizedBox(height: 20),
-                                    TextFieldBlocBuilder(
-                                      obscureText: true,
-                                      textFieldBloc: loginFormBloc.password,
-                                      decoration: textFieldInputDecoration(
-                                          "Password", ""),
-                                    ),
-                                    const SizedBox(height: 20),
-                                    GestureDetector(
-                                      onTap: () {
-                                        loginFormBloc.submit();
-                                      },
-                                      child: Container(
-                                        padding: EdgeInsets.all(15),
-                                        decoration: BoxDecoration(
-                                          color: ColorConstant.primaryColor,
-                                          borderRadius:
-                                              BorderRadius.circular(10),
-                                        ),
-                                        child: Center(
-                                          child: Text(
-                                            "Sign In",
-                                            style: TextStyle(
-                                              color: ColorConstant.bgColor,
-                                            ),
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.normal,
                     ),
                   ),
                 ],
               ),
             ),
-          );
-        }),
+            const SizedBox(height: 10),
+            // Title
+            Text(
+              "Welcome back to Clothus, \n You've been missed",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: ColorConstant.primaryColor,
+                fontSize: 16,
+              ),
+            ),
+            const SizedBox(height: 50),
+            // Form Card
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: ColorConstant.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(25),
+                    topRight: Radius.circular(25),
+                  ),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: SingleChildScrollView(
+                    physics: const BouncingScrollPhysics(),
+                    child: Column(
+                      children: [
+                        const SizedBox(height: 10),
+                        Form(
+                          key: _formKey,
+                          child: Column(
+                            children: [
+                              TextFormField(
+                                initialValue: "test@example.com",
+                                onSaved: (value) {
+                                  _loginRequestModel.email = value;
+                                },
+                                // controller: emailController,
+                                keyboardType: TextInputType.emailAddress,
+                                decoration: textFieldInputDecoration(
+                                    "Email", "ex: izzat@ump.com"),
+                              ),
+                              const SizedBox(height: 20),
+                              TextFormField(
+                                initialValue: "password",
+                                onSaved: (value) {
+                                  _loginRequestModel.password = value;
+                                },
+                                //controller: passwordController,
+                                obscureText: true,
+                                decoration:
+                                    textFieldInputDecoration("Password", ""),
+                              ),
+                              const SizedBox(height: 20),
+                              GestureDetector(
+                                onTap: () {
+                                  if (validateAndSave()) {
+                                    loginProcess(_loginRequestModel);
+                                  }
+                                },
+                                child: Container(
+                                  padding: EdgeInsets.all(15),
+                                  decoration: BoxDecoration(
+                                    color: ColorConstant.primaryColor,
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  child: Center(
+                                    child: Text(
+                                      "Sign In",
+                                      style: TextStyle(
+                                        color: ColorConstant.bgColor,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  Future<void> loginProcess(LoginRequestModel loginModel) async {
+    LoadingDialog.show(context);
+    UserResponseModel responseModel = await loginBloc.login(loginModel);
+
+    LoadingDialog.hide(context);
+
+    if (responseModel.isSuccess) {
+      // Save access_token
+      await SecureSharedPreferences.write(
+          key: "access_token", value: responseModel.data!.accessToken!);
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: ((context) => HomePageScreen()),
+        ),
+      );
+    } else {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text(responseModel.message)));
+    }
+  }
+
+  bool validateAndSave() {
+    final form = _formKey.currentState;
+
+    if (form!.validate()) {
+      form.save();
+      return true;
+    }
+
+    return false;
   }
 }
